f7e3299b97607b8dfa232dd0f22ba2b1dca68faf
James Pak <jamesjiminpak@gmail.com>
2019-05-16 12:58:28 -0700

    dependencies imported, index.js set up

---
+++ friends/src/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from '@babel/template';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+
+const store = createStore(
+    rootReducer,
+    applyMiddleware(thunk, logger)
+);
+
+ReactDOM.render(
+  <Provider>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
